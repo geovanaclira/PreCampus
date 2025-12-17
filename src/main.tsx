5b7d5f133cb898aeab41945a8488ec58b906bc98
geovanaclira <geovana.liira@gmail.com>
2025-12-16 21:36:29 -0300

    feat: cards added to the events panel

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider} from 'react-router-dom'
 import './index.css'
+import { PainelEventos } from './pages/aluno/PainelEventos'
 
 const router = createBrowserRouter([
   {
@@ -13,6 +14,11 @@ const router = createBrowserRouter([
     path: '/login',
     // element: ,
   },
+
+  {
+    path: '/aluno/painel-eventos',
+    element: <PainelEventos/>,
+  }
 ])
  
 ReactDOM.createRoot(document.getElementById('root')!).render(

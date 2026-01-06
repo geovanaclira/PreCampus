811d803d2894e4984682052c169c9360abc136d6
David-Evangelist <davidevangelist.dev@gmail.com>
2026-01-05 23:58:29 -0300

    refactor(app): remove unused App.tsx

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import Register from "./pages/Register";
-import { PainelEventos } from "./pages/PainelEventos";
-
-function App() {
-  return <PainelEventos />;
-}
-
-export default App;

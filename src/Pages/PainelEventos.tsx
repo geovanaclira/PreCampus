38e849a31e08adcb3fd16b47e416172058552f15
David-Evangelist <davidevangelist.dev@gmail.com>
2026-01-05 23:51:54 -0300

    refactor(header): use HeaderContext to set page titles

--- src/Pages/PainelEventos.tsx
+++ src/Pages/PainelEventos.tsx
@@ -1,17 +1,17 @@
+import { useEffect } from "react";
 import { Card } from "../components/Cards/Card";
-import Header from "../components/HeaderMenu/Header";
+import { useHeader } from "../contexts/HeaderContext";
 
-export function PainelEventos() {
+const PainelEventos = () => {
+  const { setTitle } = useHeader();
+
+  useEffect(() => {
+    setTitle("Painel de Eventos");
+  }, []);
+
   return (
     <>
-      <Header
-        title="Painel de Eventos"
-        username="Aislan"
-        userType="Aluno"
-        role="student"
-      />
-
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 p-10 gap-4">
+      <div className="grid grid-cols-1 gap-4 p-10 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         <Card
           title="Evento 1"
           imageUrl="https://placehold.co/300x150"
@@ -43,7 +43,17 @@ export function PainelEventos() {
           onToggleFavorite={() => console.log("clicou no coração")}
           variant="aluno"
         />
+
+        <Card
+          title="Evento 4"
+          imageUrl="https://placehold.co/300x150"
+          isFavorite={false}
+          onToggleFavorite={() => console.log("clicou no coração")}
+          variant="aluno"
+        />
       </div>
     </>
   );
-}
+};
+
+export default PainelEventos;

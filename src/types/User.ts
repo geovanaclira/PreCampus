60479151ab55144ad7a6cceae502c9d7be9348b3
David-Evangelist <davidevangelist.dev@gmail.com>
2026-01-21 08:24:10 -0300

    refactor(types): split user types by role

--- src/types/User.ts
+++ src/types/User.ts
@@ -1,10 +1,44 @@
+import type { Address } from "./AddressType";
+
 export type Role = "student" | "school" | "university" | "admin";
 
-export interface User {
+export interface BaseUser {
   id: string;
   email: string;
   role: Role;
-
-  fullName?: string;
-  corporateName?: string;
 }
+
+export interface StudentUser extends BaseUser {
+  role: "student";
+  fullName: string;
+  phone: string;
+  birthDate: string;
+  monthlyIncome: number;
+  interestedCourses: string[];
+  interestedUniversities: string[];
+  address?: Address;
+  guardian?: {
+    fullName: string;
+    relationship: string;
+    email: string;
+    address?: Address;
+  };
+}
+
+/* ===================== SCHOOL ===================== */
+export interface SchoolUser extends BaseUser {
+  role: "school";
+  corporateName: string;
+  phone: string;
+  address: Address;
+}
+
+/* ===================== UNIVERSITY ===================== */
+export interface UniversityUser extends BaseUser {
+  role: "university";
+  corporateName: string;
+  phone: string;
+  description: string;
+}
+
+export type User = StudentUser | SchoolUser | UniversityUser;
